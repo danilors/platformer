a966218304aa11d30aac7562d508ca5399b49eae
Danilo Silva <danilo170@hotmail.com>
2025-01-09 00:42:47 -0300

    init anim8 code

--- main.lua
+++ main.lua
@@ -1,7 +1,16 @@
-function love.load()
+love.load = function()
+    anim8 = require "libraries.anim8.anim8"
+    sprites = {}
+    sprites.playerSheet = love.graphics.newImage('assets/sprites/playerSheet.png')
+    
+    local grid = anim8.newGrid(614, 564, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+    animations = {}
+    animations.idle = anim8.newAnimation()
+    
     wf = require "libraries.windfield.windfield"
     worldSleep = false
     world = wf.newWorld(0, 800, worldSleep)
+    world:setQueryDebugDrawing(true)
 
     world:addCollisionClass("Platform")
     world:addCollisionClass("Player" --[[ , {ignores = {'Platform'}} ]])
@@ -18,7 +27,7 @@ function love.load()
     dangerZone:setType("static")
 end
 
-function love.update(dt)
+love.update = function(dt)
     world:update(dt)
     if player.body then
         local x, y = player:getPosition()
@@ -41,12 +50,24 @@ function love.update(dt)
     end
 end
 
-function love.draw()
+love.draw = function()
     world:draw()
 end
 
 love.keypressed = function(key)
     if key == "up" then
-        player:applyLinearImpulse(0, -7000)
+        local colliders = world:queryRectangleArea(player:getX() - 40, player:getY() + 40, 80, 2, {"Platform"})
+        if #colliders > 0 then
+            player:applyLinearImpulse(0, -7000)
+        end
+    end
+end
+
+love.mousepressed = function(x, y, button)
+    if button == 1 then
+        local colliders = world:queryCircleArea(x, y, 200, {"Platform", "Danger"})
+        for i, c in ipairs(colliders) do
+            c:destroy()
+        end
     end
 end
